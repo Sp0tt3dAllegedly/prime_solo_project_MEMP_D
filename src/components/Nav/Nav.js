53bfae78239313fdf1e24cc898b2e1878db650d1
Jake Knecht <Ljknecht91@gmail.com>
2019-08-06 15:26:57 -0500

    alter Nav to reflect design intent

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,10 +5,10 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
 const Nav = (props) => (
-  <div className="nav">
-    <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
-    </Link>
+  <>
+  <div className="header-MEMPD">Mary Edworthy-McPherson Design Gallery</div>
+  
+    
     <div className="nav-right">
       <Link className="nav-link" to="/home">
         {/* Show this link if they are logged in or not,
@@ -30,7 +30,8 @@ const Nav = (props) => (
         About
       </Link>
     </div>
-  </div>
+  
+  </>
 );
 
 // Instead of taking everything from state, we just want the user
