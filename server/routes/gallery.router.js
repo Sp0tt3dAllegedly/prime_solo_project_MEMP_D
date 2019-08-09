4d5a5f2ba73a3527626d471390f97ee1fa59f682
Jake Knecht <Ljknecht91@gmail.com>
2019-08-09 10:04:37 -0500

    update comments for gallery.router.js

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -2,22 +2,27 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/*
-*** GET route for gallery_items table!
- */
+
+// ***> GET route for gallery_items table to display entire gallery!
+
 router.get('/', (req, res) => {
+    
     const sqlText = `SELECT * from "gallery_items";`;
 
     pool.query(sqlText)
-    .then( (response)=>{
-        res.send(response.rows);
-    })
-    .catch ( (error)=>{
-        console.log('error with gallery content router get...', error);
-        res.sendStatus(500);
-        
-    })
+
+        .then( (response)=>{
+            res.send(response.rows);
+        })
+
+        .catch ( (error)=>{
+            console.log('error with gallery content router get...', error);
+            res.sendStatus(500);
+            
+        })
 });
+
+// ***> GET route for gallery_items table to display item details by id!
 
 router.get('/:id', (req, res) => {
 
@@ -26,14 +31,17 @@ router.get('/:id', (req, res) => {
     const value = [req.params.id]
 
     pool.query(sqlText, value)
-    .then( (response) => {
-        res.send(response.rows[0]);
-    })    
-    .catch( (error) => {
-        res.sendStatus(500);
-        alert('could not get those details... try again later');
-    })
-}) 
+    
+        .then( (response) => {
+            res.send(response.rows[0]);
+        }) 
+
+        .catch( (error) => {
+            res.sendStatus(500);
+            alert('could not get those details... try again later');
+        })
+
+}); 
 
 /*
 **** POST route template
